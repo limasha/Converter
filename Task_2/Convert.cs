d75b8abb885650cff89c47acf60556694bd74c0c
limasha <limasha.perera.98@gmail.com>
2020-08-15 20:09:44 +0530

    add file

---
+++ Task_2/Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Convert
+    {
+        public int meter;
+        public double getValue()
+        {
+            double kilometer =(double) meter / 1000;
+            return kilometer;
+        }
+
+    }
+}
